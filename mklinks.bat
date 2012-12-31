c1682a617d43d9131f6627f15163c2ccea9aadff
Stephane Martin <stephane.martin23@gmail.com>
2012-12-31 12:18:42 +0100

    Commit original

---
+++ mklinks.bat
@@ -0,0 +1,21 @@
+d:
+cd \wamp21e\www\sitejoomla_baseannuairecommercants\components
+rmdir com_sobipro
+MKLINK /D com_sobipro D:\wamp21e\www\sitejoomla_baseannuairecommercants_git\components\com_sobipro
+pause
+cd \wamp21e\www\sitejoomla_baseannuairecommercants\modules
+rmdir mod_jmaps
+MKLINK /D mod_jmaps D:\wamp21e\www\sitejoomla_baseannuaire_git\modules\mod_jmaps
+pause
+cd \wamp21e\www\sitejoomla_baseannuairecommercants\modules
+rmdir mod_sobiextsearch 
+MKLINK /D mod_sobiextsearch D:\wamp21e\www\sitejoomla_baseannuaire_git\modules\mod_sobiextsearch
+pause
+cd \wamp21e\www\sitejoomla_baseannuairecommercants\modules
+rmdir mod_sobipro_tree
+MKLINK /D mod_sobipro_tree D:\wamp21e\www\sitejoomla_baseannuaire_git\modules\mod_sobipro_tree
+pause
+cd \wamp21e\www\sitejoomla_baseannuairecommercants\templates
+rmdir baseannuairecommercants
+MKLINK /D baseannuairecommercants D:\wamp21e\www\sitejoomla_baseannuairecommercants_git\templates\baseannuairecommercants
+pause
